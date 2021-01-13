ddc30b911207146d094ed1c90c1c40031c1283cd
Maxence <lemairemaxence@hotmail.com>
2021-01-13 11:30:23 +0100

    exo 1 2 3 ok

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+// EXO 1
+
+// let num = 851
+
+// let reverseNumber = (num) => {
+//     return (parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num))
+// }
+
+// console.log(reverseNumber(num));
+
+// EXO 2
+
+// let number = prompt("Nombre ?")
+// let result = number / 2;
+
+// let divPar2 = () => {
+//     switch (number % 2 === 0) {
+//         case true:
+//             alert(`$${number} est divisible par 2 -> ${number} : 2 = ${result}`)
+//             break;
+    
+//         default:
+//             alert("Ce nombre n'est pas divisible par 2")
+//             break;
+//     }
+//     return result
+// }
+
+// console.log(divPar2(result));
+
+// EXO 3
+
+// let mdp = prompt("Mot de passe ?")
+
+// let logIn = () => {
+//     while (mdp !== "mdp") {
+//         mdp = prompt("Mot de passe ?")
+//     }
+//     if (mdp == "mdp") {
+//         alert("Vous êtes connecté")
+//     }
+//     return mdp
+// }
+
+console.log(logIn());
+
+// EXO 4
